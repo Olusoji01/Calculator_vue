5a4620f44b14135bca8acb951b6c8693a3669fab
Ajayi Sunday Olusoji <ajayisunday64@gmail.com>
2024-03-25 20:42:38 +0100

    Updated todo and quote

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
 import TodoList from './components/TodoList.vue'
-// import RandomeQuoteGenerator from './components/RandomQuoteGenerator.vue'
-// import AmazingCalculator from './components/AmazingCalculator.vue'
+import RandomeQuoteGenerator from './components/RandomQuoteGenerator.vue'
+import AmazingCalculator from './components/AmazingCalculator.vue'
 </script>
 
 <template>
   <TodoList />
-  <!-- <RandomeQuoteGenerator/> -->
-  <!-- <AmazingCalculator/> -->
+  <RandomeQuoteGenerator />
+  <AmazingCalculator />
 </template>
 
 <style></style>
